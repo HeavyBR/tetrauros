770991f0f077a20fc23fc851421550afa8b9b68b
Matheus Bruder <matheuspercario@gmail.com>
2019-09-19 13:34:46 -0300

    Adicionando pedras.js (cada bloco é formado por um conjunto de pedras)

---
+++ pedras.js
@@ -0,0 +1,41 @@
+//Atalhos para facilitar escrita das matrizes
+var X = true;
+var l = false;
+
+varLesq = [
+            //Posição 1
+            [    [l,X,l],
+                 [l,X,l],
+                 [l,X,X]    ],
+            //Posição 2
+            [    [X,X,X],
+                 [X,l,l]    ],
+            //Posição 3
+            [    [l,X,X],
+                 [l,l,X],
+                 [l,l,X]    ],
+            //Posição 4
+            [    [l,l,X],
+                 [X,X,X]    ]
+        ];
+
+varLdir = [
+            //Posição 1
+            [   [l,X,l],
+                [l,X,l],
+                [X,X,l]     ],
+            //Posição 2
+            [   [X,X,X],
+                [l,l,X]     ],
+            //Posição 3
+            [   [X,X,l],
+                [X,l,l],
+                [X,l,l]     ],
+            //Posição 4
+            [   [X,l,l],
+                [X,X,X]     ]
+        ];
+
+
+//Cria um arraypara indexar de forma aleatória uma peça
+var todasPedras = [/*quadrado, linha, */Lesq, Ldir/*, Te, S1, S2*/];
