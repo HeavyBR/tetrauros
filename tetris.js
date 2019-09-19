c58dc17a30d79730789a9ab07e59007e68938781
Matheus Cumpian <matheus.cumpian@hotmail.com>
2019-09-19 13:07:46 -0300

    Adicionando Canvas e retângulo de teste.

---
+++ tetris.js
@@ -0,0 +1,8 @@
+//Pegando referências
+var canvas = document.getElementById("tela");
+var ctx = canvas.getContext("2d");
+var btPausar = document.getElementById("btPausar");
+var btNovo = document.getElementById("btNovo");
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(20,30,50,100);
