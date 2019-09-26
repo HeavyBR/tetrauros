bd3557e5b484ff4e3903817814ec208930df5f3e
Matheus Cumpian <matheus.cumpian@hotmail.com>
2019-09-26 08:11:44 -0300

    Mudando lógica do canvas

--- tetris.js
+++ tetris.js
@@ -1,8 +1,44 @@
-//Pegando referências
-var canvas = document.getElementById("tela");
-var ctx = canvas.getContext("2d");
-var btPausar = document.getElementById("btPausar");
-var btNovo = document.getElementById("btNovo");
+const cvs = document.getElementById('tela');
+const VAZIO = "white";
+const ctx = cvs.getContext('2d');
+const SQ = SQUARESIZE = 20;
+const LINHA = Math.floor(cvs.height / SQUARESIZE);
+const COLUNA = Math.floor(cvs.width / SQUARESIZE);;
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(20,30,50,100);
+
+
+let tabuleiro = [];
+
+
+//Criando tabuleiro e preenchendo com blocos brancos
+for(l = 0; l < LINHA; l++)
+{
+    tabuleiro[l] = [];
+    for(c = 0; c < COLUNA; c++)
+    {
+        tabuleiro[l][c] = VAZIO;
+    }
+}
+
+
+function drawBoard()
+{
+    for(l = 0; l < LINHA; l++)
+    {
+        for(c = 0; c < COLUNA; c++)
+        {
+            drawSquare(c,l,tabuleiro[l][c]);
+        }
+    }
+}
+
+drawBoard();
+
+//Functions
+function drawSquare(x,y,color)
+{
+    ctx.fillStyle = color;
+    ctx.fillRect(x * SQ,y * SQ, SQ, SQ);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x * SQ, y * SQ, SQ , SQ);
+}
